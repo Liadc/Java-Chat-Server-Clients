d31604647dddcce4b494ea9ebd6778b6c77d1379
Timor Sharabi <xposionn@gmail.com>
2018-11-05 12:17:48 +0200

    Added Timor as Branch Set serverGUI as static, startserver Method as private
    Created ClientGUI and Client
    
    added readers and writers

--- src/timor/Client.java
+++ src/timor/Client.java
@@ -2,17 +2,39 @@ package timor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client implements Runnable {
 
-    public Client(InetAddress host,int port){
+    public Client(InetAddress host,int port,String name){
         this.ip = host;
         this.port = port;
 
     }
+
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(this.ip,this.port);
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(),true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        writer.println(msg);
+    }
+
+    //Private Methods
+
+
+    //Bones
 
     private int port;
     private InetAddress ip;
@@ -21,16 +43,4 @@ public class Client implements Runnable {
     private BufferedReader reader;
     private PrintWriter writer;
 
-    @Override
-    public void run() {
-        try {
-            socket = new Socket(this.ip,this.port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void sendMsg(String msg) {
-        //create readers and writes.
-    }
 }
